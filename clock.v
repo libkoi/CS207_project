a5d394ea8890d10c2f44a1c2d786dbd5b4235805
陈宇恒(Chen Yuheng) <11711111@mail.sustc.edu.cn>
2018-12-12 18:53:25 +0800

    Create clock.v
    时钟

---
+++ clock.v
@@ -0,0 +1,87 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: SUSTech
+// Engineer: Chen Yuheng
+// 
+// Create Date: 2018/12/12 15:38:53
+// Design Name: 
+// Module Name: Clock
+// Project Name: Alarm Clock
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module clock(input clk, rst, output[5:0] hour, minute, second);
+    wire[0:0] clk_bps;
+    reg[5:0] reg_hour,reg_minute,reg_second;
+    counter u_c(clk,rst,clk_bps);
+    always @(posedge clk or posedge rst)
+        if(rst)
+        begin
+            reg_hour<=6'd0;
+            reg_minute<=6'd0;
+            reg_second<=6'd0;
+        end
+    always @(posedge clk_bps)
+    begin
+        if(reg_second==6'd59)
+            reg_second<=6'd0;
+        else
+            reg_second<=reg_second+1'b1;
+    end
+    always @(posedge clk_bps)
+        begin
+            if(reg_minute==6'd59 )
+            begin
+                if(reg_second==6'd59)
+                reg_minute<=6'd0;
+            end
+            else if(reg_second==6'd59)
+                reg_minute<=reg_minute+1'b1;
+        end
+    always @(posedge clk_bps)
+    begin
+        if(reg_hour==6'd23 )
+        begin
+            if(reg_minute==6'd59)
+            begin
+                if(reg_second==6'd59)
+                    reg_hour<=6'd0;
+            end
+        end
+        else if(reg_minute==6'd59)
+        begin
+            if(reg_second==6'd59)
+                reg_hour<=reg_hour+1'b1;
+        end        
+    end
+    assign hour=reg_hour;
+    assign minute=reg_minute;
+    assign second=reg_second;
+endmodule
+
+module counter(input clk, rst,output clk_bps);
+    reg [13:0] cnt_first, cnt_second;
+    always @(posedge clk or posedge rst)
+        if(rst)
+            cnt_first<=14'd0;
+        else if(cnt_first==14'd10000)
+            cnt_first<=14'd0;
+        else
+            cnt_first<=cnt_first+1'b1;
+    always @(posedge clk or posedge rst)
+        if(rst)
+            cnt_second<=14'd0;
+        else if(cnt_second==14'd10000)
+            cnt_second<=14'd0;
+        else if(cnt_first==14'd10000)
+            cnt_second<=cnt_second+1'b1;
+    assign clk_bps= cnt_second == 14'd10000 ? 1'b1:1'b0;
+endmodule
