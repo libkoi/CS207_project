a4fbdfa4895a6d5e6205c1b60d47074d7ed58d71
Stang <pmx42@live.cn>
2018-12-24 22:08:21 +0800

    Add 0.05s module for tube

--- source/clock.v
+++ source/clock.v
@@ -114,3 +114,24 @@ module counter(input clk, rst,output clk_bps);
             cnt_second<=cnt_second+1'b1;
     assign clk_bps= cnt_second == 14'd10000 ? 1'b1:1'b0;
 endmodule
+
+
+module counter_tube(input clk, rst,output clk_bps_tube);
+// 0.05 s per posedge
+    reg [13:0] cnt_first, cnt_second;
+    always @(posedge clk or posedge rst)
+        if(rst)
+            cnt_first<=14'd0;
+        else if(cnt_first==14'd10000)
+            cnt_first<=14'd0;
+        else
+            cnt_first<=cnt_first+1'b1;
+    always @(posedge clk or posedge rst)
+        if(rst)
+            cnt_second<=14'd0;
+        else if(cnt_second==14'd500)
+            cnt_second<=14'd0;
+        else if(cnt_first==14'd10000)
+            cnt_second<=cnt_second+1'b1;
+    assign clk_bps_tube= cnt_second == 14'd500 ? 1'b1:1'b0;
+endmodule
