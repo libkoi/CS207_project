4ffbd914fbcb3dbc19746cfcb55aec68ac3e13cf
Stang <pmx42@live.cn>
2018-12-25 15:03:38 +0800

    Revert "简并"
    This reverts commit 3812a79f9d6f75c7ae2a74755d4e10609af10458.

--- source/display.v
+++ source/display.v
@@ -34,11 +34,10 @@ reg [2:0] scan_cnt;         // scan count, 0 --> 7
 reg [31:0] cnt;
 parameter period = 200000; // 500Hz for stable
 
-//reg [6:0] Y_r;              // for selection
+reg [6:0] Y_r;              // for selection
 reg [7:0] DIG_r;            // for one digit
-//assign seg_en = {1'b1, (~Y_r[6:0])};   // dot never light
-assign seg_out = Xseg_out;
-assign seg_en = ~DIG_r;
+assign seg_en = {1'b1, (~Y_r[6:0])};   // dot never light
+assign seg_out = ~DIG_r;
 
 
 reg [3:0] hh;
@@ -72,13 +71,115 @@ begin
     else begin
         hh <= hour   / 10;
         hl <= hour   % 10;
-        mh <= minute / 10;
-        ml <= minute % 10;
-        sh <= second / 10;
-        sl <= second % 10;
+        mh <= minute / 60;
+        ml <= minute % 60;
+        sh <= second / 60;
+        sl <= second % 60;
     end
 end
 
+
+//////////////////////////////////////////////////////////
+ // display convert toolkit 2
+ // [3:0] digit --> [7:0] tube signal
+ always @(posedge clk or negedge rst)
+ 
+ begin
+    if(!rst) begin
+        hh_r <= 0;
+        hl_r <= 0;
+        mh_r <= 0;
+        ml_r <= 0;
+        sh_r <= 0;
+        sl_r <= 0;
+    end
+    else begin
+        case (hh)
+            0: hh_r <= 7'b0111111;
+            1: hh_r <= 7'b0000110;
+            2: hh_r <= 7'b1011011;
+            3: hh_r <= 7'b1001111;
+            4: hh_r <= 7'b1100110;
+            5: hh_r <= 7'b1101101;
+            6: hh_r <= 7'b1111101;
+            7: hh_r <= 7'b0100111;
+            8: hh_r <= 7'b1111111;
+            9: hh_r <= 7'b1100111;
+            default : hh_r <= 7'b0000000;
+        endcase
+      
+        case (hl)
+            0: hl_r <= 7'b0111111;
+            1: hl_r <= 7'b0000110;
+            2: hl_r <= 7'b1011011;
+            3: hl_r <= 7'b1001111;
+            4: hl_r <= 7'b1100110;
+            5: hl_r <= 7'b1101101;
+            6: hl_r <= 7'b1111101;
+            7: hl_r <= 7'b0100111;
+            8: hl_r <= 7'b1111111;
+            9: hl_r <= 7'b1100111;
+            default : hl_r <= 7'b0000000;
+        endcase
+    
+        case (mh)
+            0: mh_r <= 7'b0111111;
+            1: mh_r <= 7'b0000110;
+            2: mh_r <= 7'b1011011;
+            3: mh_r <= 7'b1001111;
+            4: mh_r <= 7'b1100110;
+            5: mh_r <= 7'b1101101;
+            6: mh_r <= 7'b1111101;
+            7: mh_r <= 7'b0100111;
+            8: mh_r <= 7'b1111111;
+            9: mh_r <= 7'b1100111;
+            default : mh_r <= 7'b0000000;
+        endcase
+    
+        case (ml)
+            0: ml_r <= 7'b0111111;
+            1: ml_r <= 7'b0000110;
+            2: ml_r <= 7'b1011011;
+            3: ml_r <= 7'b1001111;
+            4: ml_r <= 7'b1100110;
+            5: ml_r <= 7'b1101101;
+            6: ml_r <= 7'b1111101;
+            7: ml_r <= 7'b0100111;
+            8: ml_r <= 7'b1111111;
+            9: ml_r <= 7'b1100111;
+            default : ml_r <= 7'b0000000;
+        endcase
+    
+        case (sh)
+            0: sh_r <= 7'b0111111;
+            1: sh_r <= 7'b0000110;
+            2: sh_r <= 7'b1011011;
+            3: sh_r <= 7'b1001111;
+            4: sh_r <= 7'b1100110;
+            5: sh_r <= 7'b1101101;
+            6: sh_r <= 7'b1111101;
+            7: sh_r <= 7'b0100111;
+            8: sh_r <= 7'b1111111;
+            9: sh_r <= 7'b1100111;
+            default : sh_r <= 7'b0000000;
+        endcase
+    
+        case (sl)
+            0: sl_r <= 7'b0111111;
+            1: sl_r <= 7'b0000110;
+            2: sl_r <= 7'b1011011;
+            3: sl_r <= 7'b1001111;
+            4: sl_r <= 7'b1100110;
+            5: sl_r <= 7'b1101101;
+            6: sl_r <= 7'b1111101;
+            7: sl_r <= 7'b0100111;
+            8: sl_r <= 7'b1111111;
+            9: sl_r <= 7'b1100111;
+            default : sl_r <= 7'b0000000;
+        endcase
+    end    
+ end
+//////////////////////////////////////////////////////////
 
 reg clkout;
 //////////////////////////////////////////////////////////
@@ -112,7 +213,7 @@ begin
     else
         begin
             scan_cnt <= scan_cnt + 1;
-            if(scan_cnt == 3'd5) scan_cnt <= 0;
+            if(scan_cnt == 3'd7) scan_cnt <= 0;
         end
 end
 //////////////////////////////////////////////////////////
@@ -122,49 +223,38 @@ end
 always @(scan_cnt) // tube select
 begin
     case (scan_cnt)
-        3'd0 : DIG_r = 8'b0000_0001;
-        3'd1 : DIG_r = 8'b0000_0010;
-        3'd2 : DIG_r = 8'b0000_0100;
-        3'd3 : DIG_r = 8'b0000_1000;
-        3'd4 : DIG_r = 8'b0001_0000;
-        3'd5 : DIG_r = 8'b0010_0000;
+        3'b000 : DIG_r = 8'b0000_0001;
+        3'b001 : DIG_r = 8'b0000_0010;
+        3'b010 : DIG_r = 8'b0000_0100;
+        3'b011 : DIG_r = 8'b0000_1000;
+        3'b100 : DIG_r = 8'b0001_0000;
+        3'b101 : DIG_r = 8'b0010_0000;
+        3'b110 : DIG_r = 8'b0000_0000; // 8'b0100_0000;
+        3'b111 : DIG_r = 8'b0000_0000; // 8'b1000_0000;
         default: DIG_r = 8'b0000_0000;
     endcase
 end
 //////////////////////////////////////////////////////////
 
 
-reg [3:0] Q;
 
+//////////////////////////////////////////////////////////
+// display on one tube digit
 always @(scan_cnt)
 begin
     case (scan_cnt)
-        3'd0: Q = sl;
-        3'd1: Q = sh;
-        3'd2: Q = ml;
-        3'd3: Q = mh;
-        3'd4: Q = hl;
-        3'd5: Q = hh;
+        0: Y_r = sl;
+        1: Y_r = sh;
+        2: Y_r = ml;
+        3: Y_r = mh;
+        4: Y_r = hl;
+        5: Y_r = hh;
+        6: Y_r = 7'b0000000;
+        7: Y_r = 7'b0000000;
+        default Y_r = 7'b0000000;  
     endcase
+    
 end
-
-reg [7:0] Xseg_out;
-
-always @(Q)
-begin
-    case (Q[3:0])
-    	4'd0: Xseg_out<=8'b01000000;
-        4'd1: Xseg_out<=8'b01111001;
-        4'd2: Xseg_out<=8'b00100100;
-        4'd3: Xseg_out<=8'b00110000;
-        4'd4: Xseg_out<=8'b00011001;
-        4'd5: Xseg_out<=8'b00010010;
-        4'd6: Xseg_out<=8'b00000010;
-        4'd7: Xseg_out<=8'b01111000;
-        4'd8: Xseg_out<=8'b00000000;
-        4'd9: Xseg_out<=8'b00010000;
-        default:  Xseg_out<=8'b11111111;
-    endcase
-end
+//////////////////////////////////////////////////////////
 
 endmodule
