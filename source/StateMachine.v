ee0fa7a6a82e70f8e71c650ed47d664cb50dd5b7
Stang <pmx42@live.cn>
2018-12-28 20:41:56 +0800

    Revert "整理后的代码"
    This reverts commit bca90ba78499c9193e0c090068b52b25391a5c11.

--- source/StateMachine.v
+++ source/StateMachine.v
@@ -21,37 +21,77 @@
 
 
 module StateMachine(
-    input clk,//100MHz晶震时钟信号
-    input rst,//重置信号，测试用
-    input on,//声音功能开关
-    input h,//调整小时的按钮
-    input min,//调整分钟的按钮
-    output reg sound_on,//声音功能开关提示灯
-    output reg[5:0] hour,//二进制时钟小时输出
-    output reg[5:0] minute,//二进制时钟分钟输出
-    output reg[5:0] second,//二进制时钟秒输出
+    input clk,
+    input rst,
+    input change,
+   input next,//切换设置数位
+   input on,
+   input h,
+   input min,
+    output reg sound_on,
+    input [3:0] row, 
+    output [3:0] col,
+    output reg[5:0] hour,//二进制输出
+    output reg[5:0] minute,//二进制输出
+    output reg[5:0] second,//二进制输出
     output [7:0] seg_out,
-    output [7:0] seg_en,//显示器输出
-    output speak//蜂鸣器输出
+    output [7:0] seg_en,
+    output speak
+    //output reg[2:0] twink
     );
-    
+    reg state;
+    reg set_time;
     wire [5:0] reg_hour;
     wire [5:0] reg_minute;
-    wire [5:0] reg_second;//中间变量
-    
-    clock c(clk,rst,h,min,reg_hour,reg_minute,reg_second);//时钟模块
-    display d(rst,clk,reg_hour,reg_minute,reg_second,seg_out,seg_en);//显示模块
-    sound sd(on,clk,reg_hour,reg_minute,reg_second,speak);//声音模块
-    
+    wire [5:0] reg_second;
+    wire [5:0] key_hour;
+    wire [5:0] key_minute;
+    wire [2:0] twinkle;
+    reg start;
+    clock c(clk,rst,h,min,set_time,key_hour,key_minute,reg_hour,reg_minute,reg_second);
+    display d(rst,clk,reg_hour,reg_minute,reg_second,seg_out,seg_en);
+  //  naoning nao(on,clk,reg_hour,reg_minute,reg_second,speak);
+    sound sd(on,clk,reg_hour,reg_minute,reg_second,speak);
+    SetTime st(clk,rst,start,next,row,col,key_hour,key_minute,twinkle);
     always @(posedge clk)
     sound_on=on;
-    //控制声音开关
+   // SetTime st(clk,rst,start,next,row,col,key_hour,key_minute,twinkle); 
+//    always @(posedge rst or posedge change)
+//    begin
+//        if(rst)
+//            state=0;
+//        else if(change)
+//            state=~state;
+//    end
     
+//    always @(posedge clk)
+//    begin
+//        if(state)
+//        begin
+//            start=1;
+//            #100
+//            start=0;
+//        end
+//    end
+//    always @(posedge state)
+//    begin
+//        start=1'b1;      
+//    end
+//    always @(state)
+//    begin
+//        if(twinkle==3'b100)
+//        begin
+//            set_time=1'b1;
+//            #100
+//            set_time=1'b0;
+//        end
+//    end
+
+
     always @(posedge clk)
     begin
         hour=reg_hour;
         minute=reg_minute;
         second=reg_second;
     end
-    //设置时间
 endmodule
--- source/clock.v
+++ source/clock.v
@@ -19,32 +19,37 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 module clock(
-    input clk, 
-    rst,
-    h,
-    min, 
-    output reg[5:0] hour, 
-    output reg[5:0] minute, 
-    output reg[5:0] second
-    );
-    
-    wire clk_fbps;//每秒跳动四次的时钟信号
+input clk, 
+rst,
+h,
+min,
+ set_time,
+[5:0] key_hour,
+[5:0] key_minute, 
+output reg[5:0] hour, 
+output reg[5:0] minute, 
+output reg[5:0] second);
+//四个输入设置时间的输入分别代表小时的十位、个位、分钟的十位、个位。输入为A代表不用设置
+//这个模块不想处理输入时间的异常，希望传入模块已做好异常处理
+//三个输出直接用六位二进制数表示时分秒
+    wire clk_bps;
+    wire clk_fbps;
     reg[5:0] reg_hour,reg_minute,reg_second;
-    counter ct(clk,rst,clk_bps);//每秒一次的时钟信号的分频模块
-    counter_tube u_t(clk,rst,clk_fbps);//每秒四次的时钟信号的分频模块
-   
+    
+    counter u_c(clk,rst,clk_bps);
+    counter_tube u_t(clk,rst,clk_fbps);
     always @(posedge clk)
     begin
         hour=reg_hour;
         minute=reg_minute;
         second=reg_second;
-        if(rst)
-        begin
-            reg_hour<=6'b000_000;
-            reg_minute<=6'b000_000;
-            reg_second<=6'b000_000;
-        end
-    end//赋值与归零
+//        if(rst)
+//        begin
+//            reg_hour<=6'b001_010;
+//            reg_minute<=6'b000_111;
+//            reg_second<=6'b100_111;
+//        end
+    end
     
     always @(posedge clk_bps or posedge rst)
     begin
@@ -54,16 +59,37 @@ module clock(
             reg_second<=6'd0;
         else
             reg_second<=reg_second+1'b1;
-    end//秒钟跳动
-    
+    end
+    reg[2:0] mm;
+    reg[2:0] mh;
+    reg cm;
+    reg ch;
+//    always @(posedge min or posedge cm)
+//    begin
+//        mm<=mm+1;
+//        if(cm)
+//            mm<=0;
+//     end
+//    always @(posedge h)
+//        mh<=mh+1;
     reg[1:0] c1;
-    always @(posedge clk_fbps or posedge rst)
-    begin
+    always @(posedge clk_fbps or posedge rst or posedge set_time)
+        begin
         c1=c1+1;
-        if(c1==2'b00 && ~min)//当不处于调整时间模式时
+        if(c1==2'b00 && ~min)
         begin
             if(rst)
-                reg_minute<=6'b000000;   
+                reg_minute<=6'b000000;
+//            else if(min)
+//                reg_minute<=reg_minute+1;
+//            else if(min==(01))
+//            begin
+//                reg_minute<=reg_minute+1'b1;
+////                    reg_minute=key_hour;
+////                    reg_minute=reg_minute%60;
+//            end    
+            else if(set_time)
+                reg_minute<=key_minute;
             else if(reg_minute==6'd59 )
             begin
                 if(reg_second==6'd59)
@@ -71,8 +97,12 @@ module clock(
             end
             else if(reg_second==6'd59)
                 reg_minute<=reg_minute+1'b1;
+//            reg_minute<=reg_minute+mm;
+//            cm<=1;
+//            #100
+//            cm<=0;
         end
-        else if(min)//当处于调整时间模式时
+        else if(min)
         begin
             if(rst)
                 reg_minute<=6'b000000;
@@ -81,43 +111,54 @@ module clock(
             else
                 reg_minute<=reg_minute+1;
         end
-    end//分钟跳动
-    
+        end
     reg [1:0] c2;
-    always @(posedge clk_fbps , posedge rst)
+    always @(posedge clk_fbps , posedge rst , posedge set_time)
     begin
-        c2=c2+1;
-        if(~h&&c2==2'b00)//当不处于调整时间模式时
+    c2=c2+1;
+    if(~h&&c2==2'b00)
+    begin
+        if(rst)
+            reg_hour<=6'b000000;
+
+//        else if(h)
+//            reg_hour<=reg_hour+1'b1;
+//        begin
+//            reg_hour=key_hour;
+//            reg_hour=reg_hour%24;
+//        end
+        else if(set_time)
+            reg_hour<=key_hour;
+        else if(reg_hour==6'd23 )
         begin
-            if(rst)
-                reg_hour<=6'b000000;
-            else if(reg_hour==6'd23 )
-            begin
-                if(reg_minute==6'd59)
-                begin
-                    if(reg_second==6'd59)
-                        reg_hour<=6'd0;
-                end
-            end
-            else if(reg_minute==6'd59)
+            if(reg_minute==6'd59)
             begin
                 if(reg_second==6'd59)
-                    reg_hour<=reg_hour+1'b1;
-            end   
+                    reg_hour<=6'd0;
+            end
         end
-        else if(h)//当处于调整时间模式时
+        else if(reg_minute==6'd59)
+        begin
+            if(reg_second==6'd59)
+                reg_hour<=reg_hour+1'b1;
+        end   
+        end
+        else if(h)
         begin
             if(rst)
-                reg_hour<=6'b000000;
-            else if(reg_hour==6'd23)
-                reg_hour<=6'd0;
-            else
-                reg_hour<=reg_hour+1;
+                        reg_hour<=6'b000000;
+                    else if(reg_hour==6'd23)
+                        reg_hour<=6'd0;
+                    else
+                        reg_hour<=reg_hour+1;
         end     
-    end//时种跳动
+    end
+    
+    
+
 endmodule
 
-module counter(input clk, rst,output clk_bps);//每秒一次分频模块
+module counter(input clk, rst,output clk_bps);
     reg [13:0] cnt_first, cnt_second;
     always @(posedge clk or posedge rst)
         if(rst)
@@ -136,7 +177,8 @@ module counter(input clk, rst,output clk_bps);//每秒一次分频模块
     assign clk_bps= cnt_second == 14'd10000 ? 1'b1:1'b0;
 endmodule
 
-module counter_tube(input clk, rst,output clk_bps_tube);//每秒4次分频模块
+module counter_tube(input clk, rst,output clk_bps_tube);
+// 0.05 s per posedge
     reg [13:0] cnt_first, cnt_second;
     always @(posedge clk or posedge rst)
         if(rst)
